a709a3a30ac564b7d5360dcc0e33a3607ba72f7f
Ayush <dev.intern@gosharpener.com>
2024-06-04 23:53:48 +0530

    Added Setters and Getter to Session and made it an entity

--- src/main/java/org/example/userservice/models/Session.java
+++ src/main/java/org/example/userservice/models/Session.java
@@ -1,13 +1,19 @@
 package org.example.userservice.models;
 
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.userservice.models.enums.SessionStatus;
 
 import java.util.Date;
 
+@Setter
+@Getter
+@Entity
 public class Session extends BaseModel {
     private String token;
     @ManyToOne
